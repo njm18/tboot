c3fbd99e765d3a2cb16f425381884a69f82d54bf
C240390 <will.landau@lilly.com>
2017-09-13 13:12:51 -0400

    Touch up some metadata

--- R/package.R
+++ R/package.R
@@ -6,5 +6,6 @@
 #' @aliases drake
 #' @author William Michael Landau \email{will.landau@@lilly.com}
 #' @author Nathan Morris \email{morris_nathan@lilly.com}
+#' @references \url{https://github.com/wlandau-lilly/tboot}
 #' @importFrom stats optim
 NULL
